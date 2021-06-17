8e9f42842a50c949d110b7933f3bc0284df850c3
simarsher220 <simarsher220@gmail.com>
2021-06-18 02:19:05 +0530

    Update database-setup.sh

--- database-setup.sh
+++ database-setup.sh
@@ -4,6 +4,9 @@ sudo apt-get install --assume-yes gnupg apt-utils gnupg2 curl gcc
 
 sudo apt-get update
 
+pyenv install 3.9.5
+pyenv local 3.9.5
+
 sudo apt-get install --assume-yes mysql-server-8.0
 
 sudo usermod -d /var/lib/mysql mysql
